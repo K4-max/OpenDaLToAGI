deec1cad4469ac5eee859ed47bb6e15a3ae6e144
K4-max <157744968+K4-max@users.noreply.github.com>
2024-04-09 12:46:07 +0100

    Update agi.py

--- agi.py
+++ agi.py
@@ -1,7 +1,13 @@
 import pyautogui
 import cmd
 import llama_index
+import secrets
 
 class Long_term_memory:
+    def __init__(self, input):
+       self.input = input
+    def save_to_file(self):
+       file = open(f"index{secrets.token_hex(2).json)
 
 class AGI_AGENT_SHELL:
+    pass
